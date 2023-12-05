fbcf65faa6277e6f9946f65145ce2c29581e3220
gngpp <gngppz@gmail.com>
2023-12-05 11:05:20 +0800

    feat(impersonate): remove max_concurrent_streams for v118

--- examples/impersonate.rs
+++ examples/impersonate.rs
@@ -3,7 +3,7 @@ use reqwest_impersonate as reqwest;
 fn main() {
     // Build a client to mimic OkHttpAndroid13
     let client = reqwest::blocking::Client::builder()
-        .impersonate(reqwest::impersonate::Impersonate::Chrome104)
+        .impersonate(reqwest::impersonate::Impersonate::Chrome118)
         .enable_ech_grease(true)
         .permute_extensions(true)
         .cookie_store(true)
