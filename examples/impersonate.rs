c23b80bd703e15223dbd36662daaf2cee5ad0b95
gngpp <gngppz@gmail.com>
2023-12-29 19:30:49 +0800

    Update example

--- examples/impersonate.rs
+++ examples/impersonate.rs
@@ -5,7 +5,7 @@ fn main() {
     let client = reqwest::blocking::Client::builder()
         .impersonate(reqwest::impersonate::Impersonate::Chrome116)
         .proxy(reqwest::Proxy::all("socks5://192.168.1.1:1080").unwrap())
-        .enable_ech_grease(true)
+        // .enable_ech_grease(true)
         .permute_extensions(true)
         .cookie_store(true)
         .tls_info(true)
@@ -17,7 +17,16 @@ fn main() {
     // https://chat.openai.com/backend-api/models
     // https://chat.openai.com/backend-api/conversation
     // https://order.surfshark.com/api/v1/account/users?source=surfshark
-     match client.get("https://chat.openai.com/api/auth/csrf").send() {
+    match client.get("https://tls.peet.ws/api/all").send() {
+        Ok(res) => {
+            println!("{}", res.text().unwrap());
+        }
+        Err(err) => {
+            dbg!(err);
+        }
+    };
+
+    match client.get("https://chat.openai.com/api/auth/csrf").send() {
         Ok(res) => {
             println!("{}", res.text().unwrap());
         }
