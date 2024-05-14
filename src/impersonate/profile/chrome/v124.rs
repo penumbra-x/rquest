429bb1d763d5a4c37a0104efe7c03ecdc6434071
deedy5 <65482418+deedy5@users.noreply.github.com>
2024-05-14 12:52:46 +0300

    Impersonate: bugfix `chrome_123`, `chrome_124` headers

--- src/impersonate/profile/chrome/v124.rs
+++ src/impersonate/profile/chrome/v124.rs
@@ -2,10 +2,7 @@ use boring::ssl::{
     CertCompressionAlgorithm, SslConnector, SslConnectorBuilder, SslCurve, SslMethod, SslVersion,
 };
 use http::{
-    header::{
-        ACCEPT, ACCEPT_ENCODING, ACCEPT_LANGUAGE, CACHE_CONTROL, DNT, UPGRADE_INSECURE_REQUESTS,
-        USER_AGENT,
-    },
+    header::{ACCEPT, ACCEPT_ENCODING, ACCEPT_LANGUAGE, UPGRADE_INSECURE_REQUESTS, USER_AGENT},
     HeaderMap, HeaderValue,
 };
 use std::sync::Arc;
@@ -104,7 +101,6 @@ fn create_ssl_connector(h2: bool) -> SslConnectorBuilder {
 }
 
 fn create_headers(mut headers: HeaderMap) -> HeaderMap {
-    headers.insert(CACHE_CONTROL, HeaderValue::from_static("max-age=0"));
     headers.insert(
         "sec-ch-ua",
         HeaderValue::from_static(
@@ -113,22 +109,18 @@ fn create_headers(mut headers: HeaderMap) -> HeaderMap {
     );
     headers.insert("sec-ch-ua-mobile", HeaderValue::from_static("?0"));
     headers.insert("sec-ch-ua-platform", HeaderValue::from_static("\"macOS\""));
-    headers.insert(DNT, HeaderValue::from_static("1"));
     headers.insert(UPGRADE_INSECURE_REQUESTS, HeaderValue::from_static("1"));
     headers.insert(USER_AGENT, HeaderValue::from_static("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"));
     headers.insert(ACCEPT, HeaderValue::from_static("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"));
-    headers.insert("sec-fetch-site", HeaderValue::from_static("none"));
-    headers.insert("sec-fetch-mode", HeaderValue::from_static("navigate"));
-    headers.insert("sec-fetch-user", HeaderValue::from_static("?1"));
-    headers.insert("sec-fetch-dest", HeaderValue::from_static("document"));
+    headers.insert("Sec-Fetch-Site", HeaderValue::from_static("?1"));
+    headers.insert("Sec-Fetch-Mode", HeaderValue::from_static("same-site"));
+    headers.insert("Sec-Fetch-User", HeaderValue::from_static("document"));
+    headers.insert("Sec-Fetch-Dest", HeaderValue::from_static("navigate"));
     headers.insert(
         ACCEPT_ENCODING,
         HeaderValue::from_static("gzip, deflate, br, zstd"),
     );
-    headers.insert(
-        ACCEPT_LANGUAGE,
-        HeaderValue::from_static("en;q=0.8,en-GB;q=0.7,en-US;q=0.6"),
-    );
+    headers.insert(ACCEPT_LANGUAGE, HeaderValue::from_static("en-US;q=1.0"));
 
     headers
 }
