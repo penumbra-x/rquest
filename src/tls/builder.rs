5568b31cb3df741bb1f8f507f2b7858b00395263
0x676e67 <gngppz@gmail.com>
2024-09-22 08:16:04 +0800

    refactor: Rename the `client` module to `http`

--- src/tls/builder.rs
+++ src/tls/builder.rs
@@ -1,6 +1,6 @@
 #![allow(missing_debug_implementations)]
 use super::{cert_compression::CertCompressionAlgorithm, TlsResult, Version};
-use crate::client::client::HttpVersionPref;
+use crate::client::http::HttpVersionPref;
 use ::std::os::raw::c_int;
 use boring::error::ErrorStack;
 use boring::ssl::{
