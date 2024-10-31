15ead8ec5bd32e1bf47844bd6c87c463ace103db
0x676e67 <gngppz@gmail.com>
2024-10-31 14:26:34 +0800

    feat(http2): Exposing Http2Settings fields (#75)

--- src/tls/impersonate/http2.rs
+++ src/tls/impersonate/http2.rs
@@ -7,49 +7,49 @@ use typed_builder::TypedBuilder;
 pub struct Http2Settings {
     /// The initial connection window size.
     #[builder(default, setter(into))]
-    pub(crate) initial_connection_window_size: Option<u32>,
+    pub initial_connection_window_size: Option<u32>,
 
     /// The header table size.
     #[builder(default, setter(into))]
-    pub(crate) header_table_size: Option<u32>,
+    pub header_table_size: Option<u32>,
 
     /// Enable push.
     #[builder(default, setter(into))]
-    pub(crate) enable_push: Option<bool>,
+    pub enable_push: Option<bool>,
 
     /// The maximum concurrent streams.
     #[builder(default, setter(into))]
-    pub(crate) max_concurrent_streams: Option<u32>,
+    pub max_concurrent_streams: Option<u32>,
 
     /// The initial stream window size.
     #[builder(default, setter(into))]
-    pub(crate) initial_stream_window_size: Option<u32>,
+    pub initial_stream_window_size: Option<u32>,
 
     /// The max frame size
     #[builder(default, setter(into))]
-    pub(crate) max_frame_size: Option<u32>,
+    pub max_frame_size: Option<u32>,
 
     /// The maximum header list size.
     #[builder(default, setter(into))]
-    pub(crate) max_header_list_size: Option<u32>,
+    pub max_header_list_size: Option<u32>,
 
     /// Unknown setting8.
     #[builder(default, setter(into))]
-    pub(crate) unknown_setting8: Option<bool>,
+    pub unknown_setting8: Option<bool>,
 
     /// Unknown setting9.
     #[builder(default, setter(into))]
-    pub(crate) unknown_setting9: Option<bool>,
+    pub unknown_setting9: Option<bool>,
 
     /// The priority of the headers.
     #[builder(default, setter(into))]
-    pub(crate) headers_priority: Option<(u32, u8, bool)>,
+    pub headers_priority: Option<(u32, u8, bool)>,
 
     /// The pseudo header order.
     #[builder(default, setter(into))]
-    pub(crate) headers_pseudo_order: Option<[PseudoOrder; 4]>,
+    pub headers_pseudo_order: Option<[PseudoOrder; 4]>,
 
     /// The settings order.
     #[builder(default, setter(strip_option))]
-    pub(crate) settings_order: Option<[SettingsOrder; 8]>,
+    pub settings_order: Option<[SettingsOrder; 8]>,
 }
