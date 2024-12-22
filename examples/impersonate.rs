597668818094cb154f4cbc21a0b7e5d3d52524a9
0x676e67 <gngppz@gmail.com>
2024-12-22 14:25:17 +0800

    revert

--- examples/impersonate.rs
+++ examples/impersonate.rs
@@ -5,8 +5,6 @@ async fn main() -> Result<(), rquest::Error> {
     // Build a client to mimic Firefox133
     let client = rquest::Client::builder()
         .impersonate_skip_headers(Impersonate::Firefox133)
-        .with_http2_builder(|builder| builder.initial_stream_id(3))
-        .with_http1_builder(|builder| builder.http09_responses(true))
         .build()?;
 
     let resp = client.get("https://tls.peet.ws/api/all").send().await?;
