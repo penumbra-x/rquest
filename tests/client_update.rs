f72648feafe1440dc1ae942b75421faf940fff76
0x676e67 <gngppz@gmail.com>
2025-03-03 13:39:50 +0800

    tests: Add client emulation update test (#484)

--- tests/client_update.rs
+++ tests/client_update.rs
@@ -1,6 +1,7 @@
 #![cfg(not(target_arch = "wasm32"))]
 mod support;
 
+use rquest::{EmulationProvider, TlsConfig};
 use support::server;
 
 #[tokio::test]
@@ -42,6 +43,79 @@ async fn update_headers() {
                     http::HeaderValue::from_static("gzip"),
                 );
             })
+            .apply()
+            .unwrap();
+    })
+    .await
+    .unwrap();
+
+    let server = server::http(move |req| async move {
+        assert_eq!(
+            req.headers().get(http::header::ACCEPT_ENCODING).unwrap(),
+            "gzip"
+        );
+        http::Response::default()
+    });
+
+    let url = format!("http://{}", server.addr());
+    let resp = client.get(url).send().await.unwrap();
+    assert!(resp.status().is_success());
+    assert!(client.headers().contains_key(http::header::ACCEPT_ENCODING));
+}
+
+#[tokio::test]
+async fn update_emulation() {
+    let _ = env_logger::try_init();
+    let server = server::http(move |req| async move {
+        assert_eq!(
+            req.headers().get(http::header::ACCEPT).unwrap(),
+            "application/json"
+        );
+        http::Response::default()
+    });
+
+    let client = rquest::Client::new();
+
+    client
+        .update()
+        .emulation(
+            EmulationProvider::builder()
+                .tls_config(TlsConfig::default())
+                .default_headers({
+                    let mut headers = http::HeaderMap::new();
+                    headers.insert(
+                        http::header::ACCEPT,
+                        http::HeaderValue::from_static("application/json"),
+                    );
+                    headers
+                })
+                .build(),
+        )
+        .apply()
+        .unwrap();
+
+    let url = format!("http://{}", server.addr());
+    let resp = client.get(url).send().await.unwrap();
+    assert!(resp.status().is_success());
+    assert!(client.headers().contains_key(http::header::ACCEPT));
+
+    let client2 = client.clone();
+    tokio::spawn(async move {
+        client2
+            .update()
+            .emulation(
+                EmulationProvider::builder()
+                    .tls_config(TlsConfig::default())
+                    .default_headers({
+                        let mut headers = http::HeaderMap::new();
+                        headers.insert(
+                            http::header::ACCEPT_ENCODING,
+                            http::HeaderValue::from_static("gzip"),
+                        );
+                        headers
+                    })
+                    .build(),
+            )
             .apply()
             .unwrap();
     })
