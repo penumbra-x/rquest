4a5419b56d57a54b1cfde121fee9f41acb6c411f
0x676e67 <gngppz@gmail.com>
2025-03-03 14:12:05 +0800

    tests: Add client cloned test (#485)

--- tests/client_update.rs
+++ tests/client_update.rs
@@ -135,3 +135,72 @@ async fn update_emulation() {
     assert!(resp.status().is_success());
     assert!(client.headers().contains_key(http::header::ACCEPT_ENCODING));
 }
+
+#[tokio::test]
+async fn updatea_cloned() {
+    let _ = env_logger::try_init();
+    let server = server::http(move |req| async move {
+        assert_eq!(
+            req.headers().get(http::header::ACCEPT).unwrap(),
+            "application/json"
+        );
+        http::Response::default()
+    });
+
+    let client = rquest::Client::new();
+
+    client
+        .update()
+        .emulation(
+            EmulationProvider::builder()
+                .tls_config(TlsConfig::default())
+                .default_headers({
+                    let mut headers = http::HeaderMap::new();
+                    headers.insert(
+                        http::header::ACCEPT,
+                        http::HeaderValue::from_static("application/json"),
+                    );
+                    headers
+                })
+                .build(),
+        )
+        .apply()
+        .unwrap();
+
+    let url = format!("http://{}", server.addr());
+    let resp = client.get(url).send().await.unwrap();
+    assert!(resp.status().is_success());
+    assert!(client.headers().contains_key(http::header::ACCEPT));
+
+    let client2 = client.cloned();
+    client2
+        .update()
+        .emulation(
+            EmulationProvider::builder()
+                .tls_config(TlsConfig::default())
+                .default_headers({
+                    let mut headers = http::HeaderMap::new();
+                    headers.insert(
+                        http::header::ACCEPT_ENCODING,
+                        http::HeaderValue::from_static("gzip"),
+                    );
+                    headers
+                })
+                .build(),
+        )
+        .apply()
+        .unwrap();
+
+    let server = server::http(move |req| async move {
+        assert_ne!(
+            req.headers().get(http::header::ACCEPT_ENCODING),
+            Some(&http::HeaderValue::from_static("gzip"))
+        );
+        http::Response::default()
+    });
+
+    let url = format!("http://{}", server.addr());
+    let resp = client.get(url).send().await.unwrap();
+    assert!(resp.status().is_success());
+    assert!(!client.headers().contains_key(http::header::ACCEPT_ENCODING));
+}
