62d6266f425e83ad0998d1b2f290cb56d44df93f
0x676e67 <gngppz@gmail.com>
2025-03-22 12:46:35 +0800

    docs: update library examples

--- src/lib.rs
+++ src/lib.rs
@@ -49,48 +49,51 @@
 //! The `websocket` module provides a way to upgrade a connection to a websocket.
 //!
 //! ```rust,no_run
-//! use futures_util::{SinkExt, StreamExt, TryStreamExt};
-//! use rquest_util::Emulation;
-//! use rquest::{Client, Message, Utf8Bytes};
+//!use futures_util::{SinkExt, StreamExt, TryStreamExt};
+//!use http::header;
+//!use rquest::{Client, Message};
+//!use std::time::Duration;
 //!
-//! #[tokio::main]
-//! async fn main() -> Result<(), rquest::Error> {
-//!     // Build a client
-//!     let websocket = Client::builder()
-//!         .emulation(Emulation::Firefox136)
-//!         .build()?
-//!         .websocket("wss://echo.websocket.org")
-//!         .send()
-//!         .await?
-//!         .into_websocket()
-//!         .await?;
+//!#[tokio::main]
+//!async fn main() -> Result<(), rquest::Error> {
+//!    // Build a client
+//!    let client = Client::builder()
+//!        .cert_verification(false)
+//!        .connect_timeout(Duration::from_secs(10))
+//!        .build()?;
 //!
-//!     let (mut tx, mut rx) = websocket.split();
+//!    // Use the API you're already familiar with
+//!    let websocket = client
+//!        .websocket("wss://echo.websocket.org")
+//!        .header(header::USER_AGENT, env!("CARGO_PKG_NAME"))
+//!        .send()
+//!        .await?;
 //!
-//!     tokio::spawn(async move {
-//!         for i in 1..11 {
-//!             if let Err(err) = tx
-//!                 .send(Message::Text(Utf8Bytes::from(format!(
-//!                     "Hello, World! #{i}"
-//!                 ))))
-//!                 .await
-//!             {
-//!                 eprintln!("failed to send message: {err}");
-//!             }
-//!         }
-//!     });
+//!    assert_eq!(websocket.version(), http::Version::HTTP_11);
 //!
-//!     while let Some(message) = rx.try_next().await? {
-//!         if let Message::Text(text) = message {
-//!             println!("received: {text}");
-//!         }
-//!     }
+//!    let (mut tx, mut rx) = websocket.into_websocket().await?.split();
 //!
-//!     Ok(())
-//! }
+//!    tokio::spawn(async move {
+//!        for i in 1..11 {
+//!            if let Err(err) = tx.send(Message::text(format!("Hello, World! {i}"))).await {
+//!                eprintln!("failed to send message: {err}");
+//!            }
+//!        }
+//!    });
+//!
+//!    while let Some(message) = rx.try_next().await? {
+//!        if let Message::Text(text) = message {
+//!            println!("received: {text}");
+//!        }
+//!    }
+//!
+//!    Ok(())
+//!}
 //! ```
 //!
 //! ## Making a GET request
+//! 
+//! Making a GET request is simple.
 //!
 //! ```rust
 //! # async fn run() -> Result<(), rquest::Error> {
